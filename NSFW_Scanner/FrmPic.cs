3995f19b7f072053d83e11a0855497975a85a7ed
Zichen Luo <iluo@163.com>
2019-04-10 01:27:59 +0800

    Create FrmPic form

---
+++ NSFW_Scanner/FrmPic.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace FileScanner
+{
+    public partial class FrmPic : Form
+    {
+        public static string PicPath;
+        public FrmPic()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPic_Load(object sender, System.EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(PicPath);
+            Application.DoEvents();
+        }
+    }
+}
